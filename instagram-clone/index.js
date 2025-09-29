396d2d70094ea1f06f463b6b366857840a3c1820
Said Salas <saidsalastech@gmail.com>
2025-09-29 11:19:02 -0600

    instagram project 2.0

--- instagram-clone/index.js
+++ instagram-clone/index.js
@@ -7,7 +7,8 @@ const posts = [
         username: "vincey1853",
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        postImage: "images/post-vangogh.jpg",
+        alt: "A painting by Vincent Van Gogh",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -17,7 +18,8 @@ const posts = [
         username: "gus1819",
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        postImage: "images/post-courbet.jpg",
+        alt: "A painting by Gustave Courbet",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -27,24 +29,118 @@ const posts = [
         username: "jd1735",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        postImage: "images/post-ducreux.jpg",
+        alt: "A self-portrait by Joseph Ducreux",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
 ]
 
 posts.forEach(post => {
+
+    let i = 0
+    function likes(){
+        if (i === 0){
+             post.likes += 1
+            likesEl.innerHTML = `${post.likes} likes`
+            i += 1
+            heartButton.src = "images/liked-heart.svg"
+            
+        }
+        else {
+            post.likes -= 1
+            likesEl.innerHTML = `${post.likes} likes`
+            i -= 1
+            heartButton.src = "images/unliked-heart.svg"
+        }
+    }
+
     const postSection = document.createElement("section")
 
     const divOne = document.createElement("div")
     divOne.classList.add("sub-cont-one")
     
     const profilePhoto = document.createElement("img")
-    profilePhoto.src("images/avatar-vangogh.jpg")
-    profilePhoto.alt("Profile photo of Vincent Van Gogh")
     profilePhoto.classList.add("profile-photo")
-    divOne.appendChild("profilePhoto")
+    profilePhoto.src = post.profile
+
+    divOne.appendChild(profilePhoto)
+
+    const divSubOne = document.createElement("div")
+    divSubOne.classList.add("sub-sub-cont-one")
+
+    const accountName = document.createElement("h1")
+    accountName.classList.add("account-name")
+    accountName.innerHTML = post.name
+
+    const accountLocation = document.createElement("h2")
+    accountLocation.classList.add("account-location")
+    accountLocation.innerHTML = post.location
+
+    divSubOne.appendChild(accountName)
+    divSubOne.appendChild(accountLocation)
+
+    divOne.appendChild(divSubOne)
+
+    postSection.appendChild(divOne)
+
+    const postImage = document.createElement("img")
+    postImage.classList.add("img-post")
+    postImage.src = post.postImage
+    postImage.alt = post.alt
+
+    postImage.addEventListener("click", likes)
+
+    postSection.appendChild(postImage)
+
+    const divTwo = document.createElement("div")
+    divTwo.classList.add("sub-cont-two")
+
+    const divSubTwo = document.createElement("div")
+    divSubTwo.classList.add("sub-sub-cont-two")
+
+    let heartButton = document.createElement("img")
+    heartButton.classList.add("img-btns")
+    heartButton.src = "images/unliked-heart.svg"
+    
+    heartButton.addEventListener("click", likes)
+    
+
+    const commentButton =  document.createElement("img")
+    commentButton.classList.add("img-btns")
+    commentButton.src = "images/icon-comment.png"
+
+    const dmButton = document.createElement("img")
+    dmButton.classList.add("img-btns")
+    dmButton.src = "images/icon-dm.png"
+
+    divSubTwo.appendChild(heartButton)
+    divSubTwo.appendChild(commentButton)
+    divSubTwo.appendChild(dmButton)
+
+    divTwo.appendChild(divSubTwo)
+
+    const likesEl = document.createElement("p")
+    likesEl.classList.add("likes-el")
+    likesEl.innerHTML = `${post.likes} likes`
+
+
+    divTwo.appendChild(likesEl)
+
+    const commentEl = document.createElement("p")
+    commentEl.classList.add("comment-el")
+    commentEl.innerHTML = `<span class="user-el">${post.username} </span> ${post.comment}`
+
+    divTwo.appendChild(commentEl)
+
+    postSection.appendChild(divTwo)
+
+    const mainContainer = document.getElementById("home-page")
+    mainContainer.appendChild(postSection)
+    
 })
+
+
 
 /* <section>
                 <div class="sub-cont-one">
