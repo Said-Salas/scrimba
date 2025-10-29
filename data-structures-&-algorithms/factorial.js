32b9bcb128be826db0615bd0b3cbfaa410be87cf
Said Salas <saidsalastech@gmail.com>
2025-10-29 10:16:29 -0600

    still working on improved recursive function to calculate factorial

--- data-structures-&-algorithms/factorial.js
+++ data-structures-&-algorithms/factorial.js
@@ -10,32 +10,32 @@ let sum = 0
 // console.log(factorialCalculator(0))
 
 //with recursion
-let factorial = 0
-const recursiveFactorial = number => {
-    if (number === 0){
+    let factorial = 0
+    const recursiveFactorial = number => {
+        if (number === 0){
+            return factorial
+        }
+        else if (factorial === 0){
+            factorial = number * (number - 1)
+        }
+        else if(number === 1){
+            factorial = factorial 
+        }
+        else {
+            factorial = factorial * (number - 1)
+        }
+        number = number - 1 
+        recursiveFactorial(number)
         return factorial
     }
-    else if (factorial === 0){
-        factorial = number * (number - 1)
-    }
-    else if(number === 1){
-        factorial = factorial 
-    }
-    else {
-        factorial = factorial * (number - 1)
-    }
-    number = number - 1 
-    recursiveFactorial(number)
-    return factorial
-}
 
 // console.log(recursiveFactorial(10))
 const improvedRecursiveFactorial = number => {
-    if (number === 2) {
-        return number
+    if (number === 1) {
+        return number * (number + 1)
     }
     improvedRecursiveFactorial(number - 1)
-    return number * (number - 1)
+    return number * (number + 1 )
 }
 
 console.log(improvedRecursiveFactorial(10))
