fe0aa372324da140dbe125dac4fa0bc16ccb87ba
Said Salas <saidsalastech@gmail.com>
2025-10-28 22:31:08 -0600

    partial progress in improved recursive function to calculate factorial

--- data-structures-&-algorithms/factorial.js
+++ data-structures-&-algorithms/factorial.js
@@ -1,11 +1,11 @@
-// const factorialCalculator = number => {
-// let sum = 0
-//   for(let i = number - 1; i >= 1; i--){
-//     sum = number * i
-//     number = sum
-//   }
-//   return sum
-// }
+const factorialCalculator = number => {
+let sum = 0
+  for(let i = number - 1; i >= 1; i--){
+    sum = number * i
+    number = sum
+  }
+  return sum
+}
 
 // console.log(factorialCalculator(0))
 
@@ -29,4 +29,13 @@ const recursiveFactorial = number => {
     return factorial
 }
 
-console.log(recursiveFactorial(10))
+// console.log(recursiveFactorial(10))
+const improvedRecursiveFactorial = number => {
+    if (number === 2) {
+        return number
+    }
+    improvedRecursiveFactorial(number - 1)
+    return number * (number - 1)
+}
+
+console.log(improvedRecursiveFactorial(10))
