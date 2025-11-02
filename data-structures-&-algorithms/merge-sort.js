04aa6d95464246e8dcab0714b95f767ad83e7797
Said Salas <saidsalastech@gmail.com>
2025-11-02 12:14:03 -0600

    progress on merge-sort algorithm

---
+++ data-structures-&-algorithms/merge-sort.js
@@ -0,0 +1,75 @@
+const mergeSort = array => {
+    if(array.length <= 1) {
+        return array
+    }
+}
+
+const findMidPoint = array => {
+    const midPointIndex = Math.floor(array.length / 2)
+    return midPointIndex
+}
+
+// console.log(findMidPoint([1,2,3]))
+
+const splitArray = array => {
+    const midPoint = Math.floor(array.length / 2)
+    const leftHalf = array.slice(0, midPoint)
+    const rightHalf = array.slice(midPoint, array.length)
+
+    return [leftHalf, rightHalf]
+}
+
+console.log(splitArray([1, 2, 3, 4, 5]))
+
+
+
+/*
+Algorithm: Merge Sort
+Input: array of numbers
+Output: a new array with the numbers sorted in ascending order
+
+Steps:
+1. If there is one or no number in the array:
+2.   Return the array. It's already sorted.
+3. Find the index of the mid point in the array.
+4. Get a copy of the left half of the array.
+5. Get a copy of the right half of the array.
+6. Sort the left half.
+7. Sort the right half.
+8. Merge the two sorted halves and return the result.
+*/
+
+/*
+Challenge:
+***********
+
+Implement the first algorithm in JavaScript. 
+
+
+Notes:
+******
+
+1. Use recursion.
+2. You may find `Math.floor()` and the array `slice()` methods useful.
+*/
+
+
+/*
+Algorithm: Merge Two Sorted Arrays
+Input: two sorted arrays of numbers, left and right
+Output: a new array of the two arrays merged
+
+Steps:
+1. Init an empty result array.
+2. Init a pointer for the left half.
+3. Init a pointer for the right half.
+4. Loop over both arrays until we reached the end of one of them:
+5.   If the current number in the left half is less than the current number in the right half:
+6.     Add it to the result.
+7.     Increase the left pointer.
+8.   Else:
+9.     Add the current number from the right half to the result.
+10.    Increase the right pointer.
+11. After the loop is over, add whatever is left in either halves to the result.
+12. Return the result array.
+*/
