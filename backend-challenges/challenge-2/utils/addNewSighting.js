e50b4f4e7545cf315646f83417810e6db1684358
Said Salas <saidsalastech@gmail.com>
2025-11-03 14:53:39 -0600

    push new sighting functionality completed

--- backend-challenges/challenge-2/utils/addNewSighting.js
+++ backend-challenges/challenge-2/utils/addNewSighting.js
@@ -8,25 +8,8 @@ export async function addNewSighting(newSighting) {
     const data = await getData()
     data.push(newSighting)
 
-    const updatedFile = await fs.writeFile(pathJSON, JSON.stringify(data, null, '\t'), 'utf8')
-    //I AM NOT SEEING MY NEW SIGHTING IN THE APP, GIVE ME A HINT
-/*
-    1. Get the existing data (remember, this will already be a JS array)
-    2. Push the new sighting to it
-    3. Write data to the file.
-    4. Add the new sighting and check out the 'read' page.
-    5. Throw an error (in the catch block) if any of 1-3 above fail.
-    
-    To write data:
-    Import fs/promises
-    Use the writeFile method with the following:
-      - the relative path to the file 
-      - The data (what should we do to this data first?)
-      - The encoding 'utf8'
-    
-    Bonus: figure out how to prettify the JSON!
-    Remember to uncomment the import statement in routeHandler.js!!
-*/
+    await fs.writeFile(pathJSON, JSON.stringify(data, null, '\t'), 'utf8')
+
   } catch (err) {
       throw new Error(`Error: ${err}`)
   }
