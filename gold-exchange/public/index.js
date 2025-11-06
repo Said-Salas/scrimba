1b231499611d0e8954cee671382b9a1cc2e522d1
Said Salas <saidsalastech@gmail.com>
2025-11-06 12:35:37 -0600

    progress on gold exchanger

--- gold-exchange/public/index.js
+++ gold-exchange/public/index.js
@@ -2,3 +2,16 @@ const inputEl = document.getElementById('investment-amount')
 inputEl.addEventListener('input', function() {
     if (this.value < 0) this.value = 0
 })
+
+const priceValue =  document.getElementById('price-display')
+
+setInterval(async () => {
+    try {
+        const rawData = await fetch('/api/gold-price')
+    const goldPrice = await rawData.json()
+    console.log(goldPrice)
+    } catch (err) {
+        console.error('Error fetching gold price: ', err)
+    }
+}, 10000)
+
--- gold-exchange/server.js
+++ gold-exchange/server.js
@@ -1,11 +1,25 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
 import 'dotenv/config'
+import fetch from 'node-fetch'
 
 const PORT = 8000
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
+    if (req.url === '/api/gold-price' && req.method === 'GET') {
+        try {
+            const apiResponse = await fetch(`https://api.metalpriceapi.com/v1/latest?api_key=${process.env.GOLD_API_KEY}&base=EUR&currencies=EUR,XAU,XAG`)
+            const data = await apiResponse.json()
+                console.log(data)
+                res.setHeader('Content-Type', 'application/json')
+                res.end(JSON.stringify(data))
+        } catch (err) {
+            res.writeHead(500, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify( { error: 'Failed to fetch gold price' }))
+        }
+        return
+    }
     return await serveStatic(req, res, __dirname)
 })
 
