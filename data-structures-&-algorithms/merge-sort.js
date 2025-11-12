91392d8b338c84b4cf68ea2caa44aaa11ad290f1
Said Salas <saidsalastech@gmail.com>
2025-11-11 18:00:08 -0600

    more progress on merge sort algorithm

--- data-structures-&-algorithms/merge-sort.js
+++ data-structures-&-algorithms/merge-sort.js
@@ -1,12 +1,32 @@
+const sortArray = array => {
+    for (let i = 0; i < array.length; i++) {
+        for (let m = 0; m < array.length - 1; m++) {
+            if (array[m] > array[m + 1]) {
+                const temp = array[m]
+                array[m] = array[m + 1]
+                array[m + 1] = temp
+            }
+        }
+    }
+    return array
+}
+
 const mergeSort = array => {
     if (array.length <= 1) return array
 
     const midPoint = Math.floor(array.length / 2)
-    const leftHalf = array.slice((0, midPoint))
+    const leftHalf = array.slice(0, midPoint)
     const rightHalf = array.slice(midPoint, array.length)
 
+    const sortedLeftHalf = sortArray(leftHalf)
+    const sortedRightHalf = sortArray(rightHalf)
+
+    const mergedArray = [...sortedLeftHalf, ...sortedRightHalf]
+
+    return mergedArray
 }
 
+console.log(mergeSort([5, 4, 3, 2, 9, 8, 7, 6]))
 
 /*
 Algorithm: Merge Sort
