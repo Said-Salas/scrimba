499b9dc2267aa8e19fdf504533e0bc0623d5abc0
Said Salas <saidsalastech@gmail.com>
2025-11-17 20:59:06 -0600

    progress on backend challenges (express)

--- backend-challenges/challenge-3/server.js
+++ backend-challenges/challenge-3/server.js
@@ -5,28 +5,35 @@ const PORT = 8000
 
 const app = express()
 
+const equalStrings = (a, b) => a?.toLowerCase() === b?.toLowerCase()
+const isTrue = param => param?.toLowerCase() === 'true'
+const boolMatch = (dataBool, queryBool) => dataBool === isTrue(queryBool)
+
 app.get('/api', (req, res) => {
+    let results = startups
 
-  let filteredData = startups
+    const { industry, country, continent, is_seeking_funding: funding, has_mvp } = req.query
 
-  const { industry, country, continent, is_seeking_funding: funding, has_mvp } = req.query
+    results = results.filter(data => [
+        (!industry || equalStrings(data.industry, industry)),
+        (!country || equalStrings(data.country, country)),
+        (!continent || equalStrings(data.continent, continent)),
+        (!funding || boolMatch(data.is_seeking_funding, funding)),
+        (!has_mvp || boolMatch(data.has_mvp, has_mvp))
+    ].every(Boolean))
 
-  const equalStrings = (a, b) => a?.toLowerCase() === b?.toLowerCase()
-  const isTrue = param => param?.toLowerCase() === 'true'
-  const boolMatch = (dataBool, queryBool) => dataBool === isTrue(queryBool)
+    res.json(results)
+})
 
-  const results = filteredData.filter(data => [
-       (!industry || equalStrings(data.industry, industry)),
-       (!country || equalStrings(data.country, country)),
-       (!continent || equalStrings(data.continent, continent)),
-       (!funding || boolMatch(data.is_seeking_funding, funding)),
-       (!has_mvp || boolMatch(data.has_mvp, has_mvp))
-  ].every(Boolean))
+app.get('/api/:field/:term', (req, res) => {
+    let results = startups
 
-  res.json(results)
-
+    const { field, term } = req.params
+    results = results.filter(data => {
+        return field === 'is_seeking_funding' || field === 'has_mvp' ? boolMatch(data[field], term) : equalStrings(data[field], term)
+    })
+    
+    res.json(results)
 })
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
-
-
