6599e05da1cb52d2fe3d100cab48a1487d532cab
Said Salas <saidsalastech@gmail.com>
2025-11-19 22:46:23 -0600

    understanding better connection between Databases and servers

--- backend-challenges/challenge-4/seedTable.js
+++ backend-challenges/challenge-4/seedTable.js
@@ -3,22 +3,37 @@ import { open } from 'sqlite'
 import path from 'node:path'
 import { vinyl } from './data.js'
 
-const seedTable = async () => {
-    const db = await open({
-        filename: path.join('database.db'),
-        driver: sqlite3.Database
-    })
+async function seedTable() {
+ 
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
 
-    try {   
+  db.serialize(() => {
+    const insert = db.prepare('INSERT INTO products (title, artist, price, image, year, genre, stock) VALUES (?, ?, ?, ?, ?, ?, ?)'
+    )
 
-        console.log('All records inserted successfully')
+    try {
+        db.run('BEGIN TRANSACTION')
+        for (const record of vinyl) {
+          insert.run([record.title, record.artist, record.price, record.image, record.year, record.genre])
+        }
     } catch (err) {
 
-        console.error('Error inserting data: ', err.message)
-    } finally {
-        await db.close()
-        console.log("Database connection closed")
     }
+  })
+
+  try {
+    //
+    console.log('All records inserted successfully.')
+  } catch (err) {
+    //
+    console.error('Error inserting data:', err.message)
+  } finally {
+    await db.close()
+    console.log('Database connection closed.')
+  }
 }
 
 seedTable()
