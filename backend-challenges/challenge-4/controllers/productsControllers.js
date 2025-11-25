9188951adaa4f2e007e23a6be717ca888bf6900d
Said Salas <saidsalastech@gmail.com>
2025-11-25 13:05:47 -0600

    working on the full stack path

--- backend-challenges/challenge-4/controllers/productsControllers.js
+++ backend-challenges/challenge-4/controllers/productsControllers.js
@@ -1,13 +1,17 @@
-import { getConnection } from "../db/db"
+import { getConnection } from '../db/db.js'
 
-export async function getGenres(){
-	
+export async function getGenres(req, res) {
+
+  try {
+	const db = await getConnection()
+    const rows = await getConnection().all("SELECT DISTINCT genre FROM products")
+    const genres = rows.map(row => row.genre)
+    res.json(genres)
+  } catch (err) {
+      res.status(500).json({error: 'Failed to fetch genres', details: err.message})
+  }
 }
 
-export async function getProducts(){
-
+export async function getProducts() {
   console.log('products')
-
 }
-
-
