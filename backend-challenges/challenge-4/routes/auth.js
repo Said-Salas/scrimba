cf686053109454e66633849fa7c1fc3296a07539
Said Salas <saidsalastech@gmail.com>
2025-12-04 11:14:09 -0600

    backend challenges done for now. I learned about auth

--- backend-challenges/challenge-4/routes/auth.js
+++ backend-challenges/challenge-4/routes/auth.js
@@ -4,6 +4,6 @@ import express from 'express'
 export const authRouter = express.Router()
 
 authRouter.post('/register', registerUser)
-authRouter.post('/login', loginUser)
+authRouter.post('/login', loginUser) 
 authRouter.get('/logout', logoutUser)
 
