0582440c3f258ee0a1e99856f5a1a6a61e079843
Said Salas <saidsalastech@gmail.com>
2025-12-05 12:47:06 -0600

    react-progress 3

--- react-challenges/challenge-1/src/main.jsx
+++ react-challenges/challenge-1/src/main.jsx
@@ -1,64 +1,41 @@
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import reactLogo from './assets/react.png'
 
-import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
-
-function MyAwesomeNavbar() {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">MyAwesomeNavbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarsExample03">
-                    <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form role="search">
-                        <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                    </form>
-                </div>
-            </div>
-        </nav>
-    )
-}
-
-function MainContent() {
-    return (
-      <h1>React is great!</h1>
-    )
-}
-
-/**
- * Challenge:
- * Create your very first custom React component!
- * Call it "MainContent", and have it return a simple
- * h1 element that says "React is great!"
- * 
- * Afterward, render it on the line below MyAwesomeNavbar
- */
-
+const root = createRoot(document.getElementById('root'))
 root.render(
-    <div>
-        <MyAwesomeNavbar />
-        <MainContent/>
-    </div>
+	<main>
+		<img src={reactLogo} style={{ width: '2.5rem', padding: '0 0 0 1rem'}}/>
+		<h1>Fun facts about React</h1>
+		<ul>
+			<li>
+				Was first released in 2013
+			</li>
+			<li>
+				Was originally created by Jordan Walke
+			</li>
+			<li>
+				Has well over 100K stars on GitHub
+			</li>
+			<li>
+				Is maintained by Meta
+			</li>
+			<li>
+				Powers thousands of enterprise apps, including mobile apps
+			</li>
+		</ul>
+	</main>
 )
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
+
+We'll be adding more styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="react-logo.png"` in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
