79e10443a4c8ade5877fc5903250d915a4cf5129
Said Salas <saidsalastech@gmail.com>
2025-12-06 09:57:04 -0600

    rp9

---
+++ react-challenges/challenge-1/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+	<footer>
+		© 2025 Said Salas Development. All rights reserved.
+	</footer>
+)
--- react-challenges/challenge-1/src/main.jsx
+++ react-challenges/challenge-1/src/main.jsx
@@ -1,40 +1,11 @@
 import { createRoot } from "react-dom/client"
-import reactLogo from './assets/react.png'
+import Header from "./components/Header"
+import Main from "./components/Main"
+import { Footer } from "./components/Footer"
 
 const root = createRoot(document.getElementById("root"))
 
-const Header = () => (
-	<header className="header-el">
-		<img src={reactLogo} alt="React logo" className="react-logo"/>
-		<nav>
-			<ul className="nav-list">
-				<li>Pricing</li>
-				<li>About</li>
-				<li>Contact</li>
-			</ul>
-		</nav>
-	</header>
-)
 
-const Main = () => (
-	<main>
-		<h1>
-			Fun facts about React
-		</h1>
-		<ol>
-			<li>React is a popular library, so I will be able to
-			fit in with all the coolest devs out there! 😎</li>
-			<li>I am more likely to get a job as a front end developer
-			if I know React</li>
-		</ol>
-	</main>
-)
-
-const Footer = () => (
-	<footer>
-		© 2025 Said Salas Development. All rights reserved.
-	</footer>
-)
 
 function Page() {
     return (
