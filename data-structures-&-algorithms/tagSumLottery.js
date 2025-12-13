41036e4fcefc2d2d4a9ebc1b2a223ff477ae9466
Said Salas <saidsalastech@gmail.com>
2025-12-13 09:46:31 -0600

    improved version of tagSumLottery problem.

--- data-structures-&-algorithms/tagSumLottery.js
+++ data-structures-&-algorithms/tagSumLottery.js
@@ -7,25 +7,46 @@ const communications = [
     { tag: 24, name: "Karen the Cryol" }
   ]
 
-export const tagSumLottery = (communications, lotteryNumber) => {
+  export const tagSumLottery = (communications, lotteryNumber) => {
+    const mapDinos = new Map()
+    const winners = []
+    const usedTags = []
+    for (const dino of communications) {
+         mapDinos.set(dino.tag, dino.name)
+    }
+    for (const dino of mapDinos.keys()) {
+        if (mapDinos.has(lotteryNumber - dino) && (mapDinos.has(lotteryNumber - dino) !== dino)) {
+             if (!usedTags.some(subArr => subArr.includes(dino) || subArr.includes(lotteryNumber - dino))) {
+                 winners.push([mapDinos.get(dino), mapDinos.get(lotteryNumber - dino)].toSorted())
+                 usedTags.push([dino, lotteryNumber - dino])
+             }
+        }
+    }
+    if (winners.length > 0) {
+        console.log(winners)
+        return winners
+    } return 'No winners'
+  }
+  
+
+export const tagSumLotteryBetter = (communications, lotteryNumber) => {
   const mapDinos = new Map()
   const winners = []
-  const usedTags = []
-  for (const dino of communications) {
-       mapDinos.set(dino.tag, dino.name)
-  }
-  for (const dino of mapDinos.keys()) {
-      if (mapDinos.has(lotteryNumber - dino) && (mapDinos.has(lotteryNumber - dino) !== dino)) {
-           if (!usedTags.some(subArr => subArr.includes(dino) || subArr.includes(lotteryNumber - dino))) {
-               winners.push([mapDinos.get(dino), mapDinos.get(lotteryNumber - dino)].toSorted())
-               usedTags.push([dino, lotteryNumber - dino])
-           }
-      }
+  for (let i = 0; i < communications.length; i++) {
+    const difference = lotteryNumber - communications[i].tag
+    if (mapDinos.has(difference)) {
+        winners.push([mapDinos.get(difference), communications[i].name].toSorted())
+    } else {
+        mapDinos.set(communications[i].tag, communications[i].name)
+    }
   }
   if (winners.length > 0) {
-      console.log(winners)
+    console.log(winners)
       return winners
-  } return 'No winners'
+  } return "No winners"
 }
 
 tagSumLottery(communications, 50)
+tagSumLotteryBetter(communications, 50)
+
+
