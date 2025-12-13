f713bb23e048523928d127bbdcb7e650e649c7ad
Said Salas <saidsalastech@gmail.com>
2025-12-13 09:20:32 -0600

    tagSumLottery challenge done

--- data-structures-&-algorithms/tagSumLottery.js
+++ data-structures-&-algorithms/tagSumLottery.js
@@ -10,14 +10,16 @@ const communications = [
 export const tagSumLottery = (communications, lotteryNumber) => {
   const mapDinos = new Map()
   const winners = []
+  const usedTags = []
   for (const dino of communications) {
        mapDinos.set(dino.tag, dino.name)
   }
   for (const dino of mapDinos.keys()) {
       if (mapDinos.has(lotteryNumber - dino) && (mapDinos.has(lotteryNumber - dino) !== dino)) {
-          if (!winners.includes([mapDinos.get(dino), mapDinos.get(lotteryNumber - dino)].toSorted())) {
+           if (!usedTags.some(subArr => subArr.includes(dino) || subArr.includes(lotteryNumber - dino))) {
                winners.push([mapDinos.get(dino), mapDinos.get(lotteryNumber - dino)].toSorted())
-          }
+               usedTags.push([dino, lotteryNumber - dino])
+           }
       }
   }
   if (winners.length > 0) {
@@ -27,44 +29,3 @@ export const tagSumLottery = (communications, lotteryNumber) => {
 }
 
 tagSumLottery(communications, 50)
-
-// export const tagSumLottery = (communications, lotteryNumber) => {
-//     const winners = []
-//     const mapDinos = new Map()
-//     for (const dino of communications) {
-//         mapDinos.set(dino.tag, dino.name)
-//     }
-//     for (const dino of communications) {
-//         for (const tag of mapDinos.keys()) {
-//             if (dino.tag != tag) {
-//                 if (dino.tag + tag === lotteryNumber) {
-//                     winners.push([dino.name, mapDinos.get(tag)].toSorted())
-//                 }
-//             }
-//         }
-//     }
-
-//     if (winners.length > 0) {
-//       console.log(winners)
-//        return winners
-//     }
-//     return 'No winners'
-// }
-
-
-
-// export function tagSumLottery(communications, lotteryNumber) {
-//     const winners = []
-//     for (let i = 0; i < communications.length; i++) {
-//       for (let j = i + 1; j < communications.length; j++) {
-//         if (communications[i].tag + communications[j].tag === lotteryNumber) {
-//           winners.push([communications[i].name, communications[j].name].toSorted())
-//         }
-//       }
-//     }
-//     if (winners.length > 0) {
-//       return winners
-//     }
-//     return "No winners"
-// }
-
