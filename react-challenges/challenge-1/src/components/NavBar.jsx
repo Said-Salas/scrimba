e445cef66ef29cb48753c35ceb39f599799b7e07
Said Salas <saidsalastech@gmail.com>
2025-12-14 10:01:16 -0600

    react progress

--- react-challenges/challenge-1/src/components/NavBar.jsx
+++ react-challenges/challenge-1/src/components/NavBar.jsx
@@ -1,6 +1,23 @@
 import reactLogo from '../assets/react.png'
 
-export const NavBar = () => (
-	<h1>Nav bar goes here</h1>
-)
+export const NavBar = () => {
+	return (
+		<header className='header-el'>
+			<nav className='nav-el'>
+				<img src={reactLogo} alt='React logo' className='react-logo'/>
+				<h1>ReactFacts</h1>
+			</nav>
+		</header>
+	)	
+}
 
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
