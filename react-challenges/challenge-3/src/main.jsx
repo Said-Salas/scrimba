8a9c034fdc91b278e17b470fdf9a9bf339256fb6
Said Salas <saidsalastech@gmail.com>
2025-12-19 08:18:50 -0600

    Travel jorunal progress

--- react-challenges/challenge-3/src/main.jsx
+++ react-challenges/challenge-3/src/main.jsx
@@ -5,13 +5,3 @@ import { App } from './App'
 createRoot(document.getElementById('root')).render(
   <App />
 )
-/**
- * Challenge:
- * 
- * - Create an App component in a separate file.
- *   Import it here and render it
- * - Create a "components" folder and a Header component.
- *   Render the Header inside the App component.
- * - Follow the Travel Journal design to build the Header
- *   for our page.
- */
