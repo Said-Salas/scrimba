96ca1a73f5aa20fc13ac17b01990b36d977e50d5
Said Salas <saidsalastech@gmail.com>
2025-12-19 10:01:33 -0600

    Better entry component. Soon I will use React Reusability

--- react-challenges/challenge-3/src/components/Entry.jsx
+++ react-challenges/challenge-3/src/components/Entry.jsx
@@ -3,17 +3,20 @@ import markerIcon from '../assets/marker.svg'
 export const Entry = () => {
     return (
         <article>
-            <img src='https://scrimba.com/links/travel-journal-japan-image-url'/>
+            <div className='loc-image-container'>
+                <img src='https://scrimba.com/links/travel-journal-japan-image-url' alt='Japan image'/>
+            </div>
             <div>
                 <div className='loc-title'>
                     <img src={markerIcon} alt='Marker icon' className='marker-icon'/>
-                    <h4>JAPAN</h4>
+                    <span>JAPAN</span>
                     <a href='https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9'>View on Google Maps</a>
                 </div>
-                <h1>Mount Fuji</h1>
-                <h3>12 Jan, 2023 - 24 Jan, 2023</h3>
+                <h2>Mount Fuji</h2>
+                <p className='trip-dates'>12 Jan, 2023 - 24 Jan, 2023</p>
                 <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is 
-                    the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                </p>
             </div>
         </article>
     )
