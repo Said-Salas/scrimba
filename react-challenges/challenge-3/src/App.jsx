6b3281881ea154378258a40c4b0473af3560b64d
Said Salas <saidsalastech@gmail.com>
2025-12-25 11:41:48 -0600

    reusability in React

--- react-challenges/challenge-3/src/App.jsx
+++ react-challenges/challenge-3/src/App.jsx
@@ -1,24 +1,33 @@
+import data from '../data'
 import { Header } from "./components/Header"
 import { Entry } from "./components/Entry"
 
+/**
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
+
 export const App = () => {
-  return(
+
+    const places = data.map(place => 
+        <Entry 
+            img={place.img}
+            title={place.title}
+            country={place.country}
+            maps={place.googleMapsLink}
+            dates={place.dates}
+            text={place.text}
+        />)
+
+    return(
     <>
-    	<Header />
-    	<Entry 
-        img={
-          {
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
-            alt:"Mount Fuji"
-          }
-        }
-        title="Mount Fuji"
-        country={'Japan'.toUpperCase()}
-        maps="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
-        dates="12 Jan, 2021 - 24 Jan, 2021"
-        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-      />
+        <Header />
+        {places}
     </>
-    
-  )
-}
+
+    )
+    }
--- react-challenges/challenge-3/src/components/Entry.jsx
+++ react-challenges/challenge-3/src/components/Entry.jsx
@@ -3,20 +3,24 @@ import markerIcon from '../assets/marker.svg'
 export const Entry = props => {
     const {img, title, country, maps, dates, text} =  props
     return (
-        <article>
-            <div className='loc-image-container'>
-                <img src={img.src} alt={img.alt}/>
-            </div>
-            <div>
-                <div className='loc-title'>
-                    <img src={markerIcon} alt='Marker icon' className='marker-icon'/>
-                    <span>{country}</span>
-                    <a href={maps}>View on Google Maps</a>
+        <>
+            <article>
+                <div className='loc-image-container'>
+                    <img src={img.src} alt={img.alt}/>
                 </div>
-                <h2>{title}</h2>
-                <p className='trip-dates'>{dates}</p>
-                <p>{text}</p>
-            </div>
-        </article>
+                <div>
+                    <div className='loc-title'>
+                        <img src={markerIcon} alt='Marker icon' className='marker-icon'/>
+                        <span>{country}</span>
+                        <a href={maps}>View on Google Maps</a>
+                    </div>
+                    <h2>{title}</h2>
+                    <p className='trip-dates'>{dates}</p>
+                    <p>{text}</p>
+                </div>
+                
+            </article>
+            <hr />
+        </>
     )
 }
