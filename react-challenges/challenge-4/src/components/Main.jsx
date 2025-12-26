61200b295f23ae65e8e203c5720936dd1653f127
Said Salas <saidsalastech@gmail.com>
2025-12-26 10:02:46 -0600

    rp

--- react-challenges/challenge-4/src/components/Main.jsx
+++ react-challenges/challenge-4/src/components/Main.jsx
@@ -5,15 +5,23 @@ export const Main = () => {
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    const submit = () => console.log("Form submitted")
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
 
     return (
         <main>
-            <form className="setup" onSubmit={submit}>
+            <form className="setup" onSubmit={handleSubmit}>
                 <input 
                     type="text" 
                     placeholder="e.g. oregano" 
-                    aria-label="Add ingredient"/>
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
                 <button type="submit">Add ingredient</button>
             </form>
             <ul>
