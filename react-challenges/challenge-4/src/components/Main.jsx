0dbd564054ca92fb12693b20700869b732c9ea82
Said Salas <saidsalastech@gmail.com>
2025-12-26 08:33:14 -0600

    rp

--- react-challenges/challenge-4/src/components/Main.jsx
+++ react-challenges/challenge-4/src/components/Main.jsx
@@ -1,13 +1,24 @@
 export const Main = () => {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    const submit = () => console.log("Form submitted")
+
     return (
         <main>
-            <form className="setup">
+            <form className="setup" onSubmit={submit}>
                 <input 
                     type="text" 
                     placeholder="e.g. oregano" 
                     aria-label="Add ingredient"/>
-                <button type="submit">+ Add ingredient</button>
+                <button type="submit">Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
