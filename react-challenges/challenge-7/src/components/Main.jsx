527917c1f8bf1059c36877e75939d6d4fd9ca277
Said Salas <saidsalastech@gmail.com>
2026-01-23 09:46:50 -0600

    meme gen 1.2

--- react-challenges/challenge-7/src/components/Main.jsx
+++ react-challenges/challenge-7/src/components/Main.jsx
@@ -8,22 +8,32 @@ export const Main = () => {
         imageUrl: 'http://i.imgflip.com/1bij.jpg'
     })
 
+    const handleChange = (event) => {
+        const { value, name } = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
                 <label>Top Text
                     <input
                         type="text"
-                        placeholder="One does not simply"
+                        placeholder={meme.topText}
                         name="topText"
+                        onChange={handleChange}
                     />
                 </label>
 
                 <label>Bottom Text
                     <input
                         type="text"
-                        placeholder="Walk into Mordor"
+                        placeholder={meme.bottomText}
                         name="bottomText"
+                        onChange={handleChange}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
