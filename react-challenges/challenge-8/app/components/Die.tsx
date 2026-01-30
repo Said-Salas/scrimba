a30e5964a0995845e78e19f51c8b99ac97e248fa
Said Salas <saidsalastech@gmail.com>
2026-01-29 22:33:09 -0600

    tenzies 0.2

---
+++ react-challenges/challenge-8/app/components/Die.tsx
@@ -0,0 +1,5 @@
+export const Die = ({ value }: {value: number}) => (
+    <button className="w-[6rem] h-[6rem] cursor-pointer shadow-md bg-[#FFFFFF] text-black font-bold text-2xl rounded-[0.25rem]">
+        {value}
+    </button>
+)
--- react-challenges/challenge-8/app/page.tsx
+++ react-challenges/challenge-8/app/page.tsx
@@ -1,7 +1,20 @@
+import { Die } from "./components/Die";
+
 export default function Home() {
   return (
-    <main className="w-[90%] h-[95%] bg-white">
-        Hi. This is Said
+    <main className="w-[95vw] h-[90vh] bg-white rounded-[0.625rem] flex justify-center items-center">
+      <div className="grid grid-cols-5 gap-10 w-[80%]">
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={6}/>
+        <Die value={4}/>
+        <Die value={1}/>
+        <Die value={3}/>
+        <Die value={3}/>
+        <Die value={5}/>
+        <Die value={2}/>
+      </div>
     </main>
   );
 }
