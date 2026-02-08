d844ce4df28fa3bea50db666e3cb9e526ed5a740
Said Salas <saidsalastech@gmail.com>
2026-02-07 21:46:37 -0600

    tenzies 3.6

--- react-challenges/challenge-8/app/components/Die.tsx
+++ react-challenges/challenge-8/app/components/Die.tsx
@@ -1,6 +1,6 @@
 export const Die = ({ value, held, hold }: { value: number, held: boolean, hold: () => void}) => (
     <button className={`w-[6rem] h-[6rem] cursor-pointer shadow-md ${held ? 'bg-[#59E391]' : 'bg-[#FFFFFF]'} text-black font-bold text-[2.5rem] 
-        rounded-[0.25rem]`} onClick={hold}>
+        rounded-[0.25rem]`} onClick={hold} aria-label={`Die with value ${value}, ${held ? 'die is held' : 'die not held'}`} aria-pressed={held}>
         {value}
     </button>
 )
