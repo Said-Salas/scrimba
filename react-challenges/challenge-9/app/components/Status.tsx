31fac763dc390547b3256359ad413215f1e54d36
Said Salas <saidsalastech@gmail.com>
2026-02-10 21:31:14 -0600

    assembly 0.45

---
+++ react-challenges/challenge-9/app/components/Status.tsx
@@ -0,0 +1,8 @@
+export const Status = () => {
+    return (
+        <section className='flex flex-col justify-center w-[60%] h-[5rem] bg-[#10A95B] rounded-[0.25rem] text-center mt-[2rem]'>
+            <h2 className="text-[1.75rem] font-medium mb-[-0.25rem]">You win!</h2>
+            <p className="font-semibold text-[1.25rem]">Well done!</p>
+        </section>
+    )
+}
--- react-challenges/challenge-9/app/page.tsx
+++ react-challenges/challenge-9/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Status } from "./components/Status";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Status />
     </>
   );
 }
