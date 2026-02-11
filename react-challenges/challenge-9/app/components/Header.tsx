ae749ca3288d7abfcfc3e46cdc2aef155553c61c
Said Salas <saidsalastech@gmail.com>
2026-02-10 21:13:18 -0600

    assembly 0.3

--- react-challenges/challenge-9/app/components/Header.tsx
+++ react-challenges/challenge-9/app/components/Header.tsx
@@ -1,10 +1,10 @@
 export const Header = () => {
     return (
-        <header className="w-[40%] text-center">
+        <header className="w-fit text-center">
             <h1 className="font-medium text-[2rem] text-[#F9F4DA]">
                 Assembly: Endgame
             </h1>
-            <p>
+            <p className="text-[#8E8E8E] font-medium mt-[0.5rem]">
                 Guess the word in under 8 attempts to keep the programming world safe from Assembly!
             </p>
         </header>
--- react-challenges/challenge-9/app/layout.tsx
+++ react-challenges/challenge-9/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={hankenGrotesk.variable}>
       <body
-        className='font-sans antialiased flex flex-col items-center'
+        className='font-sans antialiased flex flex-col items-center p-8'
       >
         {children}
       </body>
