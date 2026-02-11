f52b77d0cfec047b606bf6d99ee007a7c6a1dc99
Said Salas <saidsalastech@gmail.com>
2026-02-10 21:55:10 -0600

    assembly 0.5

---
+++ react-challenges/challenge-10/components/Status.jsx
@@ -0,0 +1,12 @@
+export const Status = () => {
+    return (
+        <section className="status">
+            <h1>
+                You win!
+            </h1>
+            <p>
+                Well done!
+            </p>
+        </section>
+    )
+}
--- react-challenges/challenge-9/app/components/Status.tsx
+++ react-challenges/challenge-9/app/components/Status.tsx
@@ -1,7 +1,7 @@
 export const Status = () => {
     return (
-        <section className='flex flex-col justify-center w-[60%] h-[5rem] bg-[#10A95B] rounded-[0.25rem] text-center mt-[2rem]'>
-            <h2 className="text-[1.75rem] font-medium mb-[-0.25rem]">You win!</h2>
+        <section className='flex flex-col justify-center w-[61%] h-[5rem] bg-[#10A95B] rounded-[0.25rem] text-center mt-[2rem]'>
+            <h2 className="text-[2rem] font-bold mb-[-0.25rem]">You win!</h2>
             <p className="font-semibold text-[1.25rem]">Well done!</p>
         </section>
     )
