bb749bb8b9f65d753d30f39abfebe07c7096a4ad
Said Salas <saidsalastech@gmail.com>
2026-02-10 21:19:40 -0600

    assembly 0.4

--- react-challenges/challenge-9/app/components/Header.tsx
+++ react-challenges/challenge-9/app/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
             <h1 className="font-medium text-[2rem] text-[#F9F4DA]">
                 Assembly: Endgame
             </h1>
-            <p className="text-[#8E8E8E] font-medium mt-[0.5rem]">
+            <p className="text-[#8E8E8E] font-medium mt-[0.5rem] max-w-[24rem]">
                 Guess the word in under 8 attempts to keep the programming world safe from Assembly!
             </p>
         </header>
