de341d44c6fa0ac83775389365ac7a4024eae71b
Said Salas <saidsalastech@gmail.com>
2026-02-12 21:55:30 -0600

    assembly 1.1

--- react-challenges/challenge-10/components/Word.jsx
+++ react-challenges/challenge-10/components/Word.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react"
+import { nanoid } from 'nanoid'
 
 export const Word = () => {
     const [word, setWord] = useState('react')
-    const letters = [...word]
-    const lettersEl = letters.map(letter => (
-        <span className="letter">
+    const letters = [...word].map(letter => (
+        <span className="letter" key={nanoid()}>
             {letter.toUpperCase()}
         </span>
     ))
 
     return (
         <section className="letters">
-             {lettersEl}
+             {letters}
         </section>
     )
 }
--- react-challenges/challenge-9/app/components/Word.tsx
+++ react-challenges/challenge-9/app/components/Word.tsx
@@ -4,8 +4,7 @@ import { useState } from 'react'
 
 export const Word = () => {
     const [word, setWord] = useState('react')
-    const letters = [...word]
-    const lettersEl = letters.map(letter => (
+    const letters = [...word].map(letter => (
         <span key={nanoid()} className='text-[#F9F4DA] bg-[#323232] w-[3.5rem] h-[3.5rem] border-b-2 border-[#F9F4DA]
             font-bold flex justify-center items-center text-[1.75rem]'
         >
@@ -15,7 +14,7 @@ export const Word = () => {
 
     return (
         <section className='flex gap-0.75 mt-[3.5rem]'>
-            {lettersEl}
+            {letters}
         </section>
     )
 }
