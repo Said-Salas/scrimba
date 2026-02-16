78522fdc5480737a0c5248499cf906776946bc82
Said Salas <saidsalastech@gmail.com>
2026-02-15 22:09:50 -0600

    assembly 1.22

--- react-challenges/challenge-9/app/components/Keyboard.tsx
+++ react-challenges/challenge-9/app/components/Keyboard.tsx
@@ -1,13 +1,16 @@
 export const Keyboard = () => {
-    const alphabet = ['abcdefghijklmnopqrstuvxwyz']
+    const alphabet = 'abcdefghijklmnopqrstuvxwyz'
     const lettersBtn = [...alphabet].map(letter => (
-        <button key={letter} className="text-[#1E1E1E] w-[2rem] h-[2rem] bg-[#FCBA29] border-2 border-[#D7D7D7]">
+        <button 
+            key={letter} 
+            className="text-[#1E1E1E] w-[3.5rem] h-[3.5rem] bg-[#FCBA29] border-1 border-[#D7D7D7] rounded text-[1.5rem] font-medium"
+        >
             {letter.toUpperCase()}
         </button>
     ))
 
     return (
-        <section className="w-[20rem] mt-[3rem] border-2 border-red-500 gap-0.2 ">
+        <section className="mt-[4rem] flex gap-2 w-[50%] flex-wrap justify-center">
             {lettersBtn}
         </section>
     )
