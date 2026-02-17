8dc0e10c21ba845d4986e611dac0bef9d0183243
Said Salas <saidsalastech@gmail.com>
2026-02-16 19:36:41 -0600

    assembly 1.4

--- react-challenges/challenge-9/app/components/Keyboard.tsx
+++ react-challenges/challenge-9/app/components/Keyboard.tsx
@@ -1,10 +1,11 @@
-export const Keyboard = () => {
+export const Keyboard = ({addLetter}: {addLetter: (letter: string) => void}) => {
     const alphabet = 'abcdefghijklmnopqrstuvxwyz'
     const lettersBtn = [...alphabet].map(letter => (
         <button 
             key={letter} 
             className="text-[#1E1E1E] w-[3.5rem] h-[3.5rem] bg-[#FCBA29] border-1 border-[#D7D7D7] rounded text-[1.5rem] 
                 font-medium cursor-pointer"
+            onClick={() => addLetter(letter)}
         >
             {letter.toUpperCase()}
         </button>
--- react-challenges/challenge-9/app/page.tsx
+++ react-challenges/challenge-9/app/page.tsx
@@ -1,18 +1,27 @@
+"use client"
+
 import { Header } from "./components/Header";
 import { Status } from "./components/Status";
 import { Languages } from "./components/Languages";
 import { Word } from "./components/Word";
 import { Keyboard } from "./components/Keyboard";
 import { NewGame } from "./components/NewGame";
+import { useState } from "react";
 
 export default function Home() {
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const addLetter = (letter: string) => {
+    setGuessedLetters(prevGL => [...prevGL, letter]) 
+  }
+  console.log(guessedLetters)
+
   return (
     <>
       <Header />
       <Status />
       <Languages />
       <Word />
-      <Keyboard />
+      <Keyboard addLetter={(letter: string) => addLetter(letter)}/>
       <NewGame />
     </>
   );
