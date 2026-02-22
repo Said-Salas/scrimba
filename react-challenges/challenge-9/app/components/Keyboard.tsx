cd292e0246343b3d2234e0f7383f83bd3e675727
Said Salas <saidsalastech@gmail.com>
2026-02-22 00:34:57 -0600

    assembly 1.65

--- react-challenges/challenge-9/app/components/Keyboard.tsx
+++ react-challenges/challenge-9/app/components/Keyboard.tsx
@@ -8,14 +8,12 @@ export const Keyboard = ({word}: {word: string}) => {
         const isGuessed = guessedLetters.includes(letter)
         const isCorrect = isGuessed && word.includes(letter)
         const isWrong = isGuessed && !word.includes(letter)
-        const bgColor = isCorrect ? 'bg-[#10A95B]' : isWrong ? 'bg-[#EC5D49]' : 'bg-[#FCBA29]'
 
         return (
             <button 
                 key={letter} 
                 className={`text-[#1E1E1E] w-[3.5rem] h-[3.5rem] border-1 border-[#D7D7D7] rounded text-[1.5rem] 
-                    font-medium cursor-pointer`}
-                style={{backgroundColor : 'red'}}
+                    font-medium cursor-pointer ${isCorrect ? 'bg-[#10A95B]' : isWrong ? 'bg-[#EC5D49]' : 'bg-[#FCBA29]'}`}
                 onClick={() => setGuessedLetters(prGL => [...prGL, letter])}
             >
                 {letter.toUpperCase()}
