d8e0a70d1b8b5c6040d2306af872c7a8e166468c
Said Salas <saidsalastech@gmail.com>
2026-02-22 00:21:01 -0600

    assembly 1.64

--- react-challenges/challenge-9/app/components/Keyboard.tsx
+++ react-challenges/challenge-9/app/components/Keyboard.tsx
@@ -1,25 +1,27 @@
 import { useState } from "react"
 
 export const Keyboard = ({word}: {word: string}) => {
-    const [guessedLet, setGuessedLet] = useState<string>('')
-    let match: boolean | null = null
-    const determineColor = (letter: string) => {
-        setGuessedLet(letter)
-        match = [...word].includes(guessedLet)
-    }
+   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
     
-
     const alphabet = 'abcdefghijklmnopqrstuvxwyz'
-    const lettersBtn = [...alphabet].map(letter => (
-        <button 
-            key={letter} 
-            className={`text-[#1E1E1E] w-[3.5rem] h-[3.5rem] bg-[#FCBA29] border-1 border-[#D7D7D7] rounded text-[1.5rem] 
-                font-medium cursor-pointer ${match ? 'bg-[#10A95B]' : 'bg-[#EC5D49]'}`}
-            onClick={() => determineColor(letter)}
-        >
-            {letter.toUpperCase()}
-        </button>
-    ))
+    const lettersBtn = [...alphabet].map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && word.includes(letter)
+        const isWrong = isGuessed && !word.includes(letter)
+        const bgColor = isCorrect ? 'bg-[#10A95B]' : isWrong ? 'bg-[#EC5D49]' : 'bg-[#FCBA29]'
+
+        return (
+            <button 
+                key={letter} 
+                className={`text-[#1E1E1E] w-[3.5rem] h-[3.5rem] border-1 border-[#D7D7D7] rounded text-[1.5rem] 
+                    font-medium cursor-pointer`}
+                style={{backgroundColor : 'red'}}
+                onClick={() => setGuessedLetters(prGL => [...prGL, letter])}
+            >
+                {letter.toUpperCase()}
+            </button>
+        )
+    })
 
     return (
         <section className="mt-[4rem] flex gap-2 w-[50%] flex-wrap justify-center">
