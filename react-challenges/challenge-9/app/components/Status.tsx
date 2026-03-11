b46cb0e0f241a9a782d5824d59c6d39e6c1c196d
Said Salas <saidsalastech@gmail.com>
2026-03-10 19:02:30 -0600

    assembly 1.82

--- react-challenges/challenge-9/app/components/Status.tsx
+++ react-challenges/challenge-9/app/components/Status.tsx
@@ -3,8 +3,19 @@ export const Status = ({isGameLost, isGameWon}: {isGameLost: boolean, isGameWon:
         <section className={`flex flex-col items-center justify-center w-[61%] h-[4rem] ${isGameWon ? 'bg-[#10A95B]' : isGameLost ? 'bg-[#BA2A2A]' : ''} rounded-[0.25rem] mt-[1.5rem]
             text-[#F9F4DA]`}
         >
-            <h2 className="text-[1.5rem] font-bold mb-[-0.25rem] w-fit">{isGameWon ? 'You win!' : isGameLost ? 'Game over!' : ''}</h2>
-            <p className="font-semibold text-[1.12rem] w-fit">{isGameWon ? 'Well done! 🎉' : isGameLost ? 'You lose! Better start learning Assembly 😭' : ''}</p>
+            {isGameWon ? 
+                (
+                    <>
+                        <h2 className="text-[1.5rem] font-bold mb-[-0.25rem] w-fit">You win!</h2>
+                        <p className="font-semibold text-[1.12rem] w-fit">Well done! 🎉</p>
+                    </>
+                ) : isGameLost ? (
+                    <>
+                        <h2 className="text-[1.5rem] font-bold mb-[-0.25rem] w-fit">Game over!</h2>
+                        <p className="font-semibold text-[1.12rem] w-fit">You lose! Better start learning Assembly 😭</p>
+                    </>
+                ) : null
+            }
         </section> 
     )
 }
