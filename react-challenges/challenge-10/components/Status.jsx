b07881c591224258a0f27d331ca12015c300627b
Said Salas <saidsalastech@gmail.com>
2026-03-10 19:20:55 -0600

    assembly 1.84

--- react-challenges/challenge-10/components/Status.jsx
+++ react-challenges/challenge-10/components/Status.jsx
@@ -1,28 +1,24 @@
 export const Status = ({isGameWon, isGameLost}) => {
-    const styles = {
-        backgroundColor: isGameWon ? '#10A95B' : isGameLost ? '#BA2A2A' : ''
+    const styles = { backgroundColor: isGameWon ? '#10A95B' : isGameLost ? '#BA2A2A' : '' }
+    const gameStatus = () => {
+        if (!isGameWon && !isGameLost) return null
+        if (isGameWon) return (
+            <>
+                <h1>You win!</h1>
+                <p>Well done! 🎉</p>
+            </>
+        )
+        else return (
+            <>
+                <h1>Game over!</h1>
+                <p>You lose! Better start learning Assembly 😭</p>
+            </>
+        )
     }
 
     return (
         <section className="status" style={styles}>
-            {isGameWon ? (
-                <>
-                    <h1>
-                        {isGameWon ? 'You win!' : isGameLost ? 'Game over!' : ''}
-                    </h1>
-                    <p>
-                        {isGameWon ? 'Well done! 🎉' : isGameLost ? 'You lose! Better start learning Assembly 😭' : ''}
-                    </p>
-                </>
-            ) : isGameLost ? (
-                <>
-                <h1>
-                    {isGameWon ? 'You win!' : isGameLost ? 'Game over!' : ''}
-                </h1>
-                <p>
-                    {isGameWon ? 'Well done! 🎉' : isGameLost ? 'You lose! Better start learning Assembly 😭' : ''}
-                </p></>
-            ) : null}
+            {gameStatus()}
         </section>
     )
 }
