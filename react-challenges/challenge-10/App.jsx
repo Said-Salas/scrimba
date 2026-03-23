cfb2152dff7c31c1b600b5641c07060d2c321ae5
Said Salas <saidsalastech@gmail.com>
2026-03-23 14:42:52 -0600

    assembly 1.91

--- react-challenges/challenge-10/App.jsx
+++ react-challenges/challenge-10/App.jsx
@@ -6,14 +6,19 @@ import { Keyboard } from "./components/Keyboard"
 import { NewGame } from "./components/NewGame"
 import { useState, useEffect } from "react"
 import { languages } from './languages'
+import { getRandomWord } from "./utils"
 
 export const App = () => {
-    const [word, setWord] = useState('react')
+    const [word, setWord] = useState("")
     const [guessedLetters, setGuessedLetters] = useState([])
     const wrongGuessesCount = guessedLetters.filter(letter => ![...word].includes(letter)).length
     const rightGuessesCount = guessedLetters.filter(letter => [...word].includes(letter)).length
     const [gotNewErrors, setGotNewErrors] = useState(null)
     
+    useEffect(() => {
+        setWord(getRandomWord())
+    }, [])
+
     useEffect(() => {
          if (wrongGuessesCount > 0) setGotNewErrors(true)
     }, [wrongGuessesCount])
