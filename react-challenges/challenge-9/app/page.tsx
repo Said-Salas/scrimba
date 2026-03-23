2bed9fc38c36c66ae139ac7b7161100568233950
Said Salas <saidsalastech@gmail.com>
2026-03-23 13:52:34 -0600

    assembly 1.90

--- react-challenges/challenge-9/app/page.tsx
+++ react-challenges/challenge-9/app/page.tsx
@@ -8,16 +8,22 @@ import { Keyboard } from "./components/Keyboard";
 import { NewGame } from "./components/NewGame";
 import { useState, useEffect } from "react";
 import { languages } from "@/languages"
+import { getRandomWord } from "@/utils";
 
 export default function Home() {
-  const [word, setWord] = useState('react')
+  const [word, setWord] = useState("")
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   const wrongGuessesCount = guessedLetters.filter(letter => ![...word].includes(letter)).length
   const rightGuessCount = guessedLetters.filter(letter => [...word].includes(letter)).length
   const [gotNewErrors, setGotNewErrors] = useState<boolean | null>(null)
   const isGameLost = wrongGuessesCount >= languages.length - 1 ? true : false
-  const isGameWon = [...word].every(letter => guessedLetters.includes(letter))
+  const isGameWon =
+    word.length > 0 && [...word].every(letter => guessedLetters.includes(letter))
   const isGameOver = isGameLost || isGameWon
+
+  useEffect(() => {
+    setWord(getRandomWord())
+  }, [])
 
   useEffect(() => {
     if (wrongGuessesCount > 0) setGotNewErrors(true)
